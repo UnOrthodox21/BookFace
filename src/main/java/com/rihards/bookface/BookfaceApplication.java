aacbc6e91b8c9f92ef888b6b19c0974e7e35b7e5
Rihards Edmunds Cerps <goldminelv@gmail.com>
2022-07-17 19:02:29 +0300

    Creation of the BookFace project

---
+++ src/main/java/com/rihards/bookface/BookfaceApplication.java
@@ -0,0 +1,13 @@
+package com.rihards.bookface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rihards/bookface/BookfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rihards.bookface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
